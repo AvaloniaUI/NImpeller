db755ebf01ffaddba7f15a685a8582412d807715
Timothy Miller <innerlogic4321@gmail.com>
2025-11-14 15:55:04 +0900

    Start CommandLineParser

--- samples/Sandbox/Program.cs
+++ samples/Sandbox/Program.cs
@@ -1,59 +1,61 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using CommandLine;
 using Sandbox;
 using Sandbox.Scenes;
 
 static class Program
 {
-    enum GraphicsApi
+    public enum GraphicsApi
     {
         OpenGL,
         Vulkan,
-        Metal,
-        DontTellMeThatThisIsUnreachable
+        Metal
     }
 
-    enum Scenes
+    public enum SceneType
     {
         MMark,
         Paragraph,
         CirclingSquares
     }
 
+    public class Options
+    {
+        [Option('a', "api", Required = false, Default = GraphicsApi.OpenGL, HelpText = "Graphics API to use (OpenGL, Vulkan, Metal)")]
+        public GraphicsApi Api { get; set; }
+
+        [Option('s', "scene", Required = false, Default = SceneType.MMark, HelpText = "Scene to render (MMark, Paragraph, CirclingSquares)")]
+        public SceneType Scene { get; set; }
+
+        [Option('w', "width", Required = false, Default = 800, HelpText = "Window width")]
+        public int Width { get; set; }
+
+        [Option('h', "height", Required = false, Default = 600, HelpText = "Window height")]
+        public int Height { get; set; }
+    }
+
     static void Main(string[] args)
     {
-        // Parse command-line arguments
-        var apiType = GraphicsApi.OpenGL;
-        if(args.Length > 1)
-        {
-            if (args[1].Equals("vulkan", StringComparison.OrdinalIgnoreCase))
-                apiType = GraphicsApi.Vulkan;
-            else if (args[1].Equals("opengl", StringComparison.OrdinalIgnoreCase))
-                apiType = GraphicsApi.OpenGL;
-            else if (args[1].Equals("metal", StringComparison.OrdinalIgnoreCase))
-                apiType = GraphicsApi.Metal;
-        }
+        var parser = new Parser(with => with.CaseInsensitiveEnumValues = true);
+        parser.ParseArguments<Options>(args)
+            .WithParsed(RunApplication)
+            .WithNotParsed(errors => Environment.Exit(1));
+    }
 
-        if (new Random().Next(10) > 100)
-            apiType = GraphicsApi.DontTellMeThatThisIsUnreachable;
-
-        // Parse scene type
-        Scenes sceneType = Scenes.MMark;
-        if (args.Length > 0)
+    static void RunApplication(Options options)
+    {
+        // Create the scene based on the selected type
+        IScene scene = options.Scene switch
         {
-            Enum.TryParse<Scenes>(args[0], true, out sceneType);
-        }
-
-        IScene scene = sceneType switch
-        {
-            Scenes.MMark => new MMarkScene(),
-            Scenes.Paragraph => new ParagraphScene(),
-            Scenes.CirclingSquares => new CirclingSquares(),
+            SceneType.MMark => new MMarkScene(),
+            SceneType.Paragraph => new ParagraphScene(),
+            SceneType.CirclingSquares => new CirclingSquares(),
             _ => new MMarkScene()
         };
 
         // Check if Metal is requested and we're on macOS
-        if (apiType == GraphicsApi.Metal)
+        if (options.Api == GraphicsApi.Metal)
         {
             if (!RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
@@ -63,14 +65,14 @@ static class Program
 
             var metalApp = new MetalApplication();
             metalApp.SetScene(scene);
-            metalApp.Run();
+            metalApp.Run(options.Width, options.Height);
             return;
         }
 
         // For OpenGL and Vulkan, use SDL
-        var sdlApi = apiType == GraphicsApi.OpenGL ? SdlApplication.GraphicsApi.OpenGL : SdlApplication.GraphicsApi.Vulkan;
+        var sdlApi = options.Api == GraphicsApi.OpenGL ? SdlApplication.GraphicsApi.OpenGL : SdlApplication.GraphicsApi.Vulkan;
         var sdlApp = new SdlApplication(sdlApi);
-        if (sdlApp.Initialize())
+        if (sdlApp.Initialize(options.Width, options.Height))
         {
             sdlApp.SetScene(scene);
             sdlApp.Run();
